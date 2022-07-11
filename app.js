1d522bd6baf7af32b34b349e8596bf989175e2a4
Rose Pineau <rosepineau@protonmail.com>
2022-07-11 14:06:13 -0400

    Initialize player and computer scores.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-// commit: Create game function to manage global variables.
+// commit: Initialize player and computer scores.
 
 const game = () => 
 {
+    let playerScore = 0; // may have to change to '-1' depending on how choices are scored later.
+    let computerScore = 0;
+
 
 }
