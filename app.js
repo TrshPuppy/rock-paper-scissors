236fcf9c30a2862d9aeff3b383f0d3927df16393
Rose Pineau <rosepineau@protonmail.com>
2022-07-11 14:16:12 -0400

    Add fadeOut to startGame function.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-// commit: Initialize player and computer scores.
+// commit: Add startGame function.
 
 const game = () => 
 {
     let playerScore = 0; // may have to change to '-1' depending on how choices are scored later.
     let computerScore = 0;
 
+    const startGame = () =>
+    {   
+        const playButton = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const faceOffScreen = document.querySelector('.faceoff');
 
+        playButton.addEventListener('click', () =>
+        {
+            introScreen.classList.add('fadeOut');
+        });
+    }
+    startGame();
 }
+
+// Start game
+game();
